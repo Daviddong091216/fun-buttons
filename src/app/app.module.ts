7d31a26cd9dbfbbf0cd20a1de385ed487757a21a
David Dong <daviddong201712@gmail.com>
2020-09-20 09:44:43 -0500

    The first time commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ButtonsComponent } from './buttons/buttons.component';
+import { SkillSetComponent } from './skill-set/skill-set.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonsComponent,
+    SkillSetComponent
   ],
   imports: [
     BrowserModule
