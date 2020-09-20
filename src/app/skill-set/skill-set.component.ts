fef4610678374b1471f72d930d91390b92e59f43
David Dong <daviddong201712@gmail.com>
2020-09-20 09:58:56 -0500

    Adds skills list.

--- src/app/skill-set/skill-set.component.ts
+++ src/app/skill-set/skill-set.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skill-set.component.css']
 })
 export class SkillSetComponent implements OnInit {
-
+  listHeading: string = 'Some Coding Skills I Know';
+  skills: string[] = ['Loops', 'Conditionals', 'Functions', 'Classes', 'Modules', 'Git', 'HTML/CSS'];
+  bulletType: string = 'A';
+  alternateColor: string = 'black';
+  changeColor: boolean = true;
+  
   constructor() { }
 
   ngOnInit() {
